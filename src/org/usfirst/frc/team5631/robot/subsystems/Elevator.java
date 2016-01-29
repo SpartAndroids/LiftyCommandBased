442cff4a3f689e6b02fc406d32a778dcae68bcdf
SPARTANDROID <spartans5631@gmail.com>
2016-01-29 15:07:48 -0500

    Initial Commit

---
+++ src/org/usfirst/frc/team5631/robot/subsystems/Elevator.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team5631.robot.subsystems;
+
+import org.usfirst.frc.team5631.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Elevator extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	Talon motor = RobotMap.elevatorMotor;
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    /**
+     * Raises the elevator
+     */
+    public void raise(){
+    	motor.set(1);
+    }
+    
+    /**
+     * Lowers the elevator
+     */
+    public void lower(){
+    	motor.set(-1);
+	}
+
+	/**
+	 * Stops the elevator
+	 */
+	public void stop() {
+		motor.set(0);
+	}
+}
+
